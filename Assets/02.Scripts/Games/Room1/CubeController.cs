da41d3822cfbe983b0e583a174c0ad3782016fb5
fusanova <fusanova@gmail.com>
2020-08-06 01:16:13 +0900

    Make Game1

---
+++ Assets/02.Scripts/Games/Room1/CubeController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CubeController : MonoBehaviour
+{
+    #region Public Fields
+    [Tooltip("스피어 회전 속도 조정을 위한 필드 값")]
+    public float rotateSpeed;
+    #endregion
+
+    #region Private Fields
+    [SerializeField]
+    GameObject quizCube;
+    public int materialNumber;
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    void Start()
+    {
+        if(quizCube == null)
+        {
+            quizCube = this.gameObject;
+            //Debug.LogError("<Color=Red><a>Missing</a></Color> quizSphere Reference. Please set it up in GameObject 'SphereController'", this);
+        }
+        rotateSpeed = 50f;
+    }
+
+    void Update()
+    {
+        
+        if(rotateSpeed > 0)
+        {
+            if (rotateSpeed >= 20f)
+            {
+                rotateSpeed -= 0.05f;
+            }
+            else if (rotateSpeed >= 15f)
+            {
+                rotateSpeed -= 0.02f;
+            }
+            else if (rotateSpeed >= 10f)
+            {
+                rotateSpeed -= 0.005f;
+            }
+            else if(rotateSpeed >= 7f)
+            {
+                rotateSpeed -= 0.003f;
+            }
+            else if (rotateSpeed >= 3f)
+            {
+                rotateSpeed -= 0.002f;
+            }
+            else
+            {
+                rotateSpeed -= 0.001f;
+            }
+            Random.InitState((int.Parse(this.gameObject.name) * 10 + 1 + (int)Time.time));
+            quizCube.transform.Rotate(Vector3.up * rotateSpeed * Random.value);
+            Random.InitState((int.Parse(this.gameObject.name) * 10 + 2 + (int)Time.time));
+            quizCube.transform.Rotate(Vector3.left * rotateSpeed * Random.value);
+            Random.InitState((int.Parse(this.gameObject.name) * 10 + 3 + (int)Time.time));
+            quizCube.transform.Rotate(Vector3.forward * rotateSpeed * Random.value);
+        }
+        else
+        {
+            rotateSpeed = 0f;
+            quizCube.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+    }
+
+    #endregion
+
+}
---
+++ Assets/02.Scripts/Games/Room1/Room1Manager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Room1Manager : MonoBehaviour
+{
+    public static int rangeLength = 16;
+    public int maxCube = 8;
+    public int[] ranArr = Enumerable.Range(0, rangeLength).ToArray();
+    public GameObject cube;
+    public CubeController cubeController;
+
+    void Start()
+    {
+        for (int i = 0; i < rangeLength; ++i)
+        {
+            int ranIdx = Random.Range(i, rangeLength);
+
+            int temp = ranArr[ranIdx];
+            ranArr[ranIdx] = ranArr[i];
+            ranArr[i] = temp;
+        }
+        for (int i = 0; i < maxCube; ++i)
+        {
+            //Debug.Log(ranArr[i]+"애용");
+            setCube(i + 1, ranArr[i] + 1);
+        }
+    }
+
+
+
+    public void setCube(int number, int materialId)
+    {
+        cube = GameObject.Find(number.ToString());
+        cubeController = cube.GetComponent<CubeController>();
+        cube.GetComponent<MeshRenderer>().material = Resources.Load("Room1/"+materialId, typeof(Material)) as Material;
+        cubeController.materialNumber = materialId;
+    }
+
+}
