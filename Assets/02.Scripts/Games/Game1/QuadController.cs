4afb5209eb3d80bd8aac241ddfc7fcdb3e9bebe7
fusa33 <fusanova@gmail.com>
2020-08-06 17:38:27 +0900

    Update Game1

---
+++ Assets/02.Scripts/Games/Game1/QuadController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class QuadController : MonoBehaviour
+{
+    #region Public Fields
+
+    #endregion
+
+    #region Private Fields
+    [SerializeField]
+    GameObject quizQuad;
+    public int materialNumber;
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    void Start()
+    {
+        if(quizQuad == null)
+        {
+            quizQuad = this.gameObject;
+        }
+    }
+
+    #endregion
+
+}
